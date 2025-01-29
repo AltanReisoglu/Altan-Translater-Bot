90ff96e171b871b837bdc53487b3031a766c55de
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-01-29 20:54:10 +0300

    decoder.py
    The Decoder Block Of Transformer Model

--- decoder.py
+++ decoder.py
@@ -11,9 +11,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 64 # what is the maximum context length for predictions?
-max_iters = 7000
+max_iters = 3000
 eval_interval = 100
-learning_rate = 1e-4
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 128
@@ -28,27 +28,25 @@ dropout = 0.0
 
 with open(r"C:\Users\bahaa\Downloads\Birinci Yurttaş_eos.txt", 'r', encoding='utf-8') as f:
     text = f.read()
-strings=text.split("\n")
+strings = text.split("\n")
 
-
-
-tokenizer = Tokenizer()  # num_words parametresi kaldırıldı
+# Tokenizer işlemi
+tokenizer = Tokenizer()
 tokenizer.fit_on_texts(strings)
 stoi = tokenizer.word_index
-itos=dict(zip(stoi.values(), stoi.keys()))
-
+itos = dict(zip(stoi.values(), stoi.keys()))
 vocab_size = len(stoi)
-# create a mapping from characters to integers
 
-encode_en = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode_en = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+# Metni sayılara çevirme ve padding
 sequences = tokenizer.texts_to_sequences(strings)
-result = sum(sequences, [])
-# Encode işlemini string üzerinde yapıyoruz:
-data = torch.tensor(result, dtype=torch.long)
+padsequences = pad_sequences(sequences, maxlen=block_size, padding='pre')
+
+# Tensor formatına çevirme (flatten ile tek boyuta indirgeme)
+data = torch.tensor(padsequences, dtype=torch.long).flatten()
+
 
 # data loading
-"""ef get_batch(split):
+def get_batch(split):
     # generate a sequential batch of data of inputs x and targets y
 
     # Veriyi sıralı almak için sabit bir başlangıç noktası belirleyelim
@@ -64,8 +62,8 @@ data = torch.tensor(result, dtype=torch.long)
     current_index += batch_size  # Bir sonraki batch için index'i güncelle
     
     return x, y
-"""
-def get_batch(split):
+
+"""def get_batch(split):
     global current_index
     
     # Veri bitince başa sar
@@ -87,7 +85,7 @@ def get_batch(split):
 
     current_index += batch_size  # Batch indeksini güncelle
     
-    return x, y
+    return x, y"""
 # Başlangıç indexi
 current_index = 0
 @torch.no_grad()
